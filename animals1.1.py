70af86fe9070bbe336b678f638376d4de08c4d5a
Ogirok24 <157206421+Ogirok24@users.noreply.github.com>
2024-01-27 08:17:22 +0100

    Update animals1.2.py

--- animals1.1.py
+++ animals1.1.py
@@ -3,15 +3,16 @@ class Animal:
     planet = "earth"
     beak = "has beak"
 
-    def __init__(self, name, color):
+    def __init__(self, name, color, type):
         self.name = name
         self.color = color
+        self.type = type
 
 print(Animal.kind, Animal.planet, Animal.beak)
-an = Animal(name="kiwi", color="brown")
-an1 = Animal(name="pigeon", color="grey")
-an2 = Animal(name="swan", color="white")
+an = Animal(name="kiwi", color="brown", type="chordal")
+an1 = Animal(name="pigeon", color="grey", type="columbidae")
+an2 = Animal(name="swan", color="white", type="anatidae")
 
-print(f"an {an.name},{an.color}")
-print(f"an1 {an1.name},{an1.color}")
-print(f"an2 {an2.name},{an2.color}")
+print(f"an {an.name},{an.color},{an.type}")
+print(f"an1 {an1.name},{an1.color},{an1.type}")
+print(f"an2 {an2.name},{an2.color},{an2.type}")
