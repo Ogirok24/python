7577d17d7379fcd55dbd740e430a030a7dd360cf
Ogirok24 <157206421+Ogirok24@users.noreply.github.com>
2024-02-15 17:24:52 +0200

    Add files via upload

---
+++ google.py
@@ -0,0 +1,25 @@
+class Google:
+    name = "Google"
+    function = "SearchingSystem"
+
+    def searching(self):
+        pass
+
+class YouTube(Google):
+    name = "YouTube"
+    function = "VideoHosting"
+
+    def watching_videos(self):
+        pass
+
+class YouTube_Music(YouTube):
+    name = "YouTube Music"
+    function = "AudioStreamingService"
+
+    def listening_music(self):
+        pass
+
+
+
+
+
